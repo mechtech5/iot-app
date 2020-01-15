578e230247f5bcd1dac70121dc7eaff1c0c71b70
Ayush Likhar <ayush.likhar@neosofttech.com>
2020-01-15 19:43:31 +0530

    vuex init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="columns is-mobile is-centered">
+      <div class="column is-half">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>RATING</th>
+              <th>STATUS</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(device, index) in devices" :key="index">
+              <td>{{ device.id }}</td>
+              <td>{{ device.name }}</td>
+              <td>{{ device.rating }}</td>
+              <td>{{ device.status }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="">
+          <button class="button is-link is-rounded" @click="appendForm"><i class="fas fa-plus"></i> Add Device</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {mapState, mapActions} from 'vuex';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      device: {
+        id: '',
+        name: '',
+        rating: '',
+        status: ''
+      }
+    }
+  },
+  created() {
+    this.fetchDevices();
+  },
+  computed: {
+    ...mapState(['devices'])
+  },
+  methods: {
+    ...mapActions(['fetchDevices']),
+    appendForm() {
+      let empty_device = {
+        id: this.device.id,
+        name: this.device.name,
+        rating: this.device.rating,
+        status: this.device.status,
+      };
+
+      this.devices.push(empty_device);
+
+      this.device.id = '';
+      this.device.name = '';
+      this.device.rating = '';
+      this.device.status = '';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    devices: []
   },
   mutations: {
+    setDevices: (state, payload) => state.devices = payload,
   },
   actions: {
+    fetchDevices({commit}) {
+      const devices = [
+        {
+          id: 101,
+          name: 'A101',
+          rating: 5,
+          status: true
+        },
+        {
+          id: 102,
+          name: 'A102',
+          rating: 2,
+          status: true
+        },
+        {
+          id: 103,
+          name: 'A103',
+          rating: 3,
+          status: true
+        },
+      ];
+      
+      commit('setDevices', devices);
+    }
   },
   modules: {
   }
